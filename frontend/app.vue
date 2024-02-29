06ee1a18b59f362d5567ef894f651e0d3106316a
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-02-29 14:25:29 +0800

    Added loading

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,29 +1,3 @@
-<script setup>
-const API = useRuntimeConfig().public.API;
-const form = reactive({
-  username: "",
-  password: "",
-});
-async function login() {
-  const response = await $fetch(`${API}/users/signin`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(form),
-  }).catch((e) => {
-    if (e.data) {
-      alert(e.data.message);
-    } else {
-      alert("An error occurred");
-    }
-  });
-  if (response) {
-    useCookie("token", response.access_token);
-    await navigateTo("/validated");
-  }
-}
-</script>
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator />
